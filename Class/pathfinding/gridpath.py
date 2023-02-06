bc43a4a93bdd64171a121018959b4a6974970057
florent-le-maire <lemaireflorent9@gmail.com>
2023-02-06 22:56:54 +0100

    [WIP] gridpath.py: - create basic function to find neighbor

---
+++ Class/pathfinding/gridpath.py
@@ -0,0 +1,28 @@
+from Class.pathfinding.node import Node
+
+
+class GridPath:
+    def __init__(self, height, width):
+        self.nodes = []
+        for i in range(height):
+            for j in range(width):
+                self.nodes.append(Node(i, j))
+
+    def neighbors(self, node_search: Node):
+        neighbors = []
+        for i, node in enumerate(self.nodes):
+            if len(neighbors) >= 8:
+                break
+            if node_search.is_neighbor(node):
+                neighbors.append(node)
+        return neighbors
+
+    def cost(self, from_node, to_node):
+        for i, j in self.nodes:
+            if i == from_node and j == to_node:
+                return 1
+        return float('inf')
+
+    def display_grid(self, canvas):
+        print("je suis la grille")
+        # for i, j in self.nodes:
--- Class/pathfinding/node.py
+++ Class/pathfinding/node.py
@@ -1,11 +1,17 @@
 class Node:
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     def __eq__(self, other):
-        return self.position == other.position
+        return self.x == other.x and self.y == other.y
+
+    def __str__(self) -> str:
+        return '({0}, {1})'.format(self.x, self.y)
+
+    def is_neighbor(self, other):
+        if self == other:
+            return False
+        dif_x = self.x - other.x
+        dif_y = self.y - other.y
+        return 1 >= dif_x >= -1 and 1 >= dif_y >= -1
